4bee0fe828251e5bea7c5f18fc2fd1ca2ef49995
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-08 23:11:59 +0300

    vue_18

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@
     amount: 9000,
     visible: true,
     visible1: false,
-  
-
+    hidden: true,
+    hidden1: false,
 	}
 },
 computed: {
@@ -90,7 +90,8 @@ change: function(){
     <a @click.once="shor">Сработает один раз</a>
     <p v-if="visible">Показан</p>
     <p v-if="visible1">Не показан</p>
-  
+    <p v-if="hidden">Phantomka</p>
+    <p v-if="!hidden1">Dota</p>
   </div>
 </template>
 
