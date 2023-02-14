d1cb4525fe7affb2bf65c5999c675446bafd1d8d
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-14 22:34:38 +0300

    vue_36

--- src/App.vue
+++ src/App.vue
@@ -250,20 +250,23 @@ change: function(){
 <ul v-for="smh in products" :key="smh.id">		
 	<li>{{ smh.name }}</li>		
 	</ul>
-  <li>{{ smh.name }}</li>
-
   <button class="button" @click.once="add">Задание 1</button>
   <button class="button" @click.once="delFirst">Задание 2</button>
   <button class="button" @click.once="delLast">Задание 3</button>
   <button class="button" @click.once="delSecondToLast">Задание 4</button>
   <button class="button" @click.once="sortArr">Задание 5</button>
   <button class="button" @click.once="reverseOrder">Задание 6</button>
-  <ul v-for="elem in arr">
-    <li>{{ elem }}</li>
-  </ul><br>
+  <div class="red">
+    <ul v-for="elem in arr">
+      <li>{{ elem }}</li>
+    </ul><br>
+  </div>
 </template>
 
-<style >
+<style>
+red{
+  color:red
+}
 .r {
   color: yellowgreen;
   background-color: black;
