7db62785e74a4bca845561db5f2daf857b6d2266
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-20 22:54:17 +0300

    vue_47

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@
       text_button: "def",
       text_array: "ghi",
       checked: true,
+      languages: [],
     /* arr: ['Seytumerov', 'Edem', 'Rustemovich'],
     obj: {Seytumerov:'100$', Edem:'200$', Rustemovich:'300$'},
     hrefs: [
@@ -377,6 +378,20 @@ change: function(){
   <input type="checkbox" v-model="checked" />
   &nbsp;
   <p v-if="checked">{{ checked }}</p>
+  <input type="checkbox" v-model="languages" value="english" />
+  &nbsp;
+  <p>English</p>
+  &nbsp;
+  <input type="checkbox" v-model="languages" value="russian" />
+  &nbsp;
+  <p>Russian</p>
+  <input type="checkbox" v-model="languages" value="javascript" />
+  &nbsp;
+  <p>JavaScript</p>
+  &nbsp;
+  <ul v-for="elem in languages">
+    <li>{{ elem }}</li>
+  </ul>
 </template>
 
 <style>
