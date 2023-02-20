e847d021b6ee172b6b0c0337578d7dca819d9e34
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-20 23:06:20 +0300

    vue_51

--- src/App.vue
+++ src/App.vue
@@ -87,6 +87,7 @@
         2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012,
         2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000,
       ],
+      isDisabled: false,
     /* arr: ['Seytumerov', 'Edem', 'Rustemovich'],
     obj: {Seytumerov:'100$', Edem:'200$', Rustemovich:'300$'},
     hrefs: [
@@ -499,6 +500,18 @@ change: function(){
   </select>
 
   <p>{{ selected }}</p>
+  <input
+    type="checkbox"
+    :checked="isDisabled"
+    @click="($event) => (this.isDisabled = !this.isDisabled)"
+  />
+  &nbsp;
+  <button
+    v-bind:disabled="!isDisabled"
+    @click="($event) => (this.isDisabled = !this.isDisabled)"
+  >
+    {{ isDisabled ? "input is enabled" : "input is disabled" }}
+  </button>
 </template>
 
 <style>
