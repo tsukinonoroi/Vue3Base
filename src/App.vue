efd4e456ce7820ed5f0fd6ec18e80d1caa9ff879
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-02-28 22:39:00 +0300

    vue_58

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script>
-  import Employee from './components/Employee.vue';
-
-	export default {
-		components: {
-			User
-		}
-	}
+import Employee from "./components/Employee.vue";
+export default {
+  components: {
+    Employee,
+  },
+};
 </script>
 <template>
-	<Employee />
+  <Employee/>
 </template>
+
+<style >
+.active {
+  border: 5px solid green;
+}
+.valid {
+  color: blue;
+}
+</style>
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,12 +1,33 @@
 <script>
-	export default {
-		data() {
-			return {
-				name: 'john'
-			}
-		}
-	}
+export default {
+    data() {
+        return {
+name: "Edem Seytumerov"
+        }
+    }
+}
 </script>
+
 <template>
-	{{ name }}
+   <p class="Edem Seytumerov">{{ name }}</p>
 </template>
+
+<style>
+p {
+    font-size: 1cm;
+    text-align: center;
+  text-shadow:
+    1px 1px 1px silver,
+    -1px 1px 1px silver;
+  color: white;
+  transition: all 0.3333s;
+}
+p:hover {
+    text-align: center;
+    font-size: 1.2cm;
+  text-shadow:
+    -1px -1px 1px silver,
+    1px -1px 1px silver;
+  color: white;
+}
+</style>
